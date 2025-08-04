f2fbe88338d5a3fe720c510260bb806ec31ca003
Your Name <you@example.com>
2025-08-04 14:58:12 -0400

    added methods for initializing models

--- app/utils.py
+++ app/utils.py
@@ -5,10 +5,16 @@ General purpose module for storing misc functions and utilities
 from __future__ import annotations
 
 import os
+from typing import Any
 
 import torch
 from huggingface_hub import HfFolder, whoami  # type: ignore
+from sentence_transformers import CrossEncoder, SentenceTransformer
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.utils.import_utils import is_flash_attn_2_available
+from transformers.utils.quantization_config import BitsAndBytesConfig
 
+import app.constants as const
 from app.log import app_logger
 
 
@@ -40,3 +46,101 @@ def get_cuda_name() -> str | None:
         return device
 
     return None
+
+
+def log_cuda_mem():
+    """Helper method to print the current memory used/available on cuda device"""
+    if torch.cuda.is_available():
+        gpu_id = torch.cuda.current_device()
+        total = torch.cuda.get_device_properties(gpu_id).total_memory  # type: ignore
+        reserved = torch.cuda.memory_reserved(gpu_id)
+        allocated = torch.cuda.memory_allocated(gpu_id)
+        free = reserved - allocated
+
+        app_logger.debug(f"Total memory:     {total / 1e6:.2f} MB")
+        app_logger.debug(f"Reserved memory:  {reserved / 1e6:.2f} MB")
+        app_logger.debug(f"Allocated memory: {allocated / 1e6:.2f} MB")
+        app_logger.debug(f"Free within reserved: {free / 1e6:.2f} MB")
+    else:
+        app_logger.debug("No CUDA device available.")
+
+
+def create_embedding_model(
+    model_name: str = const.EMBEDDING_MODEL_NAME, send_to_cuda: bool = True
+) -> SentenceTransformer:
+    """Initializes a reranker model"""
+    app_logger.debug(f"Attempting to initialize a embedding model: {model_name}...")
+
+    model = SentenceTransformer(model_name)
+
+    if send_to_cuda:
+        if torch.cuda.is_available():
+            model.to("cuda")
+            app_logger.debug(
+                f"Embedding model sent to cuda device: {get_cuda_name()}..."
+            )
+        else:
+            app_logger.warning(
+                f"Cuda not available! Attempt to send embedding model to cuda failed."
+            )
+
+    app_logger.debug(f"Embedding model intialization complete.")
+
+    return model
+
+
+def create_reranker_model(
+    model_name: str = const.RERANKING_MODEL_NAME, send_to_cuda: bool = True
+) -> CrossEncoder:
+    """Initializes a reranker model"""
+    app_logger.debug(f"Attempting to initialize a reranker model: {model_name}")
+
+    model = CrossEncoder(model_name)
+
+    if send_to_cuda:
+        if torch.cuda.is_available():
+            model.to("cuda")
+            app_logger.debug(f"Reranker model sent to cuda device: {get_cuda_name()}")
+        else:
+            app_logger.warning(
+                f"Cuda not available! Attempt to send reranker model to cuda failed."
+            )
+
+    app_logger.debug(f"Reranker model intialization complete.")
+
+    return model
+
+
+def create_llm(
+    model_name: str = const.LLM_MODEL_NAME, send_to_cuda: bool = True
+) -> tuple[AutoTokenizer, AutoModelForCausalLM]:
+    """Helper method to initialize an llm model and the associated auto tokenizer"""
+    app_logger.debug(f"Attempting to initialize a LLM: {model_name}...")
+
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+    )
+
+    config: dict[str, Any] = {
+        "pretrained_model_name_or_path": model_name,
+        "torch_dtype": torch.float16,
+        "device_map": "auto",
+        "quantization_config": quant_config,
+    }
+
+    if is_flash_attn_2_available() and (torch.cuda.get_device_capability(0)[0] >= 8):
+        app_logger.debug(f"Using flash_attention_2")
+        config["attn_implementation"] = "flash_attention_2"
+
+    app_logger.debug("Creating llm")
+    llm: AutoModelForCausalLM = AutoModelForCausalLM.from_pretrained(**config)  # type: ignore
+
+    app_logger.debug("Creating autotokenizer")
+    tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)  # type: ignore
+
+    app_logger.debug(f"LLM and autotokenizer intialization complete.")
+
+    return tokenizer, llm  # pyright: ignore[reportUnknownVariableType]
